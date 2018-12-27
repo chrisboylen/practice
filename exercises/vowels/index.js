a599f098e0a7facaf32b537e05269db31dd3b599
Chris Boylen <billpolster@gmail.com>
2018-12-27 15:10:11 -0500

    Add Vowels function

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
+// function vowels(str) {
+//   const checkedLetters = ["a", "e", "i", "o", "u"];
+//   let count = 0;
+
+//   str.split("").forEach(letter => {
+//     if (checkedLetters.includes(letter.toLowerCase())) {
+//       count += 1;
+//     }
+//   });
+//   return count;
+// }
 
 module.exports = vowels;
